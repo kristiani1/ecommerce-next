cb0285dc2a4559267303dd09c6208be0f784dbe0
kristiani1 <64782305+kristiani1@users.noreply.github.com>
2021-12-27 14:28:35 +0200

    initial commit

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,19 @@
+import getAllProductQuery from "../utils/queries/get-all-products"
+import fetchApi from "../utils/fetch-api"
+import { ProductConnection } from "../schema"
+
+type ReturnType = {
+  products: ProductConnection
+}
+
+const getAllProducts = async (): Promise<any> => {
+  const { data } = await fetchApi<ReturnType>({query: getAllProductQuery})
+
+  const products = data.products.edges.map(({ node: product }) => {
+    return product
+  }) ?? []
+
+  return products
+}
+
+export default getAllProducts
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react"
+import type { InferGetStaticPropsType } from "next"
+import getAllProducts from "../framework/shopify/product/get-all-products"
+
+export async function getStaticProps () {
+  const products = await getAllProducts()
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
+  }
+}
+
+export default function Home({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div>
+      { JSON.stringify(products) }
+    </div>
+  )
+}
