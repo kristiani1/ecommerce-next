3a76e5aa8dca51acef90fd83b4b777610c228659
kristiani1 <64782305+kristiani1@users.noreply.github.com>
2021-12-31 15:04:51 +0200

    styling

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react"
+import style from "./Layout.module.css"
 
 const Layout: FC = ({children}) => {
     return (
-        <div className="layout">
-            {children}
+        <div  className={style.root}>
+            <main style={{color: "var(--primary)"}}>
+                {children}
+            </main>
         </div>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app"
 import { FC } from "react"
+import "@assets/main.css"
 
 const Noop: FC = ({children}) => <>{children}</>
 
