b7e5f7e6926a47d4c0882cc3c05533cdb560bb8f
kristiani1 <64782305+kristiani1@users.noreply.github.com>
2022-01-06 09:50:08 +0200

    Sidebar component

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,16 @@
 import { FC } from "react"
 import style from "./Layout.module.css"
 import { Footer, Navbar } from "@components/common"
+import { Sidebar } from "@components/ui"
+import { CartSidebar } from "@components/cart"
 
 const Layout: FC = ({children}) => {
     return (
         <div className={style.root}>
             <Navbar/>
+            <Sidebar>
+                <CartSidebar />
+            </Sidebar>
             <main className="fit">
                 {children}
             </main>
--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react"
 import style from "./Usernav.module.css"
 import Link from "next/link"
+import { Bag as Cart, Heart } from "@components/icons"
 
 const Usernav: FC = () => {
 
@@ -8,12 +9,12 @@ const Usernav: FC = () => {
         <nav>
             <ul className={style.list}>
                 <li className={style.item}>
-                    Cart
+                    <Cart/>
                 </li>
                 <li className={style.item}>
                 <Link href="/">
                     <a>
-                        Wishlist
+                        <Heart/>
                     </a>
                 </Link>
                 </li>
